a96ac69e7eea00acdef1fb0e33a1e12186a015ea
wth <tianhao.wu@insa-lyon.fr>
2017-05-24 13:09:47 +0200

    test of EvaualteAll in test-service

--- tests/MedAnalysisServerUnitTest/tests-services.cpp
+++ tests/MedAnalysisServerUnitTest/tests-services.cpp
@@ -2,14 +2,13 @@
 #include <models/Genome.h>
 #include <services/AnalysisService.h>
 
-TEST_CASE( "Evaluating All Genomes", "[server][storage]" )
+TEST_CASE( "Evaluating All Diseases", "[server][storage]" )
 {
-    AnalysisService service = AnalysisService("../../../tests/DataTest/disease");
+    AnalysisService service = AnalysisService("../../../res/disease");
     vector<string> genes;
-    genes.push_back("GGCC");
     genes.push_back("GTAC");
+    genes.push_back("GGCC");
     Genome genome = Genome(genes);
     vector<Disease> diseases = service.evaluateAll(genome);
     REQUIRE(diseases[0].name()=="Cancer");
-    REQUIRE(diseases[1].name()=="YYSB");
 }
--- tests/MedAnalysisServerUnitTest/tests-storage.cpp
+++ tests/MedAnalysisServerUnitTest/tests-storage.cpp
@@ -3,13 +3,15 @@
 
 TEST_CASE( "Loading diseases", "[server][storage]" )
 {
-    DiseaseDAO diseaseDAO = DiseaseDAO("../../../tests/DataTest/disease");
+    DiseaseDAO diseaseDAO = DiseaseDAO("../../../res/disease");
     auto diseasesMap = diseaseDAO.findAll();
     unordered_multimap<std::string,Disease>::iterator iter;
     vector<string> diseases;
-    diseases.push_back("Cancer");
-    diseases.push_back("YYSB");
+    //diseases on the iterator qre on the opposite order
     diseases.push_back("YYDSB");
+    diseases.push_back("YYSB");
+    diseases.push_back("Cancer");
+
     int i = 0;
     for(iter=diseasesMap.begin();iter!=diseasesMap.end();iter++)
     {
@@ -20,7 +22,7 @@ TEST_CASE( "Loading diseases", "[server][storage]" )
 
 TEST_CASE( "Loading diseases by name", "[server][storage]" )
 {
-    DiseaseDAO diseaseDAO = DiseaseDAO("../../../tests/DataTest/disease");
+    DiseaseDAO diseaseDAO = DiseaseDAO("../../../tests/res/disease");
     pair<   unordered_multimap<string, Disease>::iterator,
             unordered_multimap<string, Disease>::iterator
     > disease = diseaseDAO.findByName("Cancer");
