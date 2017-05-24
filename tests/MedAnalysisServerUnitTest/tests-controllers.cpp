aff050b9804b5f3bb6950ad4ae22883224749a9a
ye_yuan <wsr10000@hotmail.com>
2017-05-24 15:17:34 +0200

    all test controller

--- tests/MedAnalysisServerUnitTest/tests-controllers.cpp
+++ tests/MedAnalysisServerUnitTest/tests-controllers.cpp
@@ -2,19 +2,55 @@
 #include <utils/utils.h>
 #define MEDANALYSIS_TEST
 #include <controllers/ListController.h>
+#include <controllers/OneController.h>
+#include <controllers/AllController.h>
+
 using Catch::Matchers::Vector::ContainsElementMatcher;
 using std::string;
 using std::vector;
 
 TEST_CASE("List Controller", "[controller][server]"){
     vector<string> str;
-    str = ListController("E:\\INSA\\3A\\GI UML\\tp_clion\\INSA-GL-Analyse-medicale\\res\\dictionnaire").doGetList("MA v1.0\r\nGET DISEASES\r\n\r\n","E:\\INSA\\3A\\GI UML\\tp_clion\\INSA-GL-Analyse-medicale\\res\\dictionnaire");
+    vector<string>input;
+    input.push_back("MA v1.0");
+    input.push_back("GET DISEASES");
+    input.push_back("");
+
+    str = ListController("E:\\INSA\\3A\\GI UML\\tp_clion\\INSA-GL-Analyse-medicale\\res\\dictionnaire").doGetList(input);
     REQUIRE(str[0]=="MA v1.0");
     REQUIRE(str[1]=="DISEASES");
     //REQUIRE(str[2]=="");
-    REQUIRE(str[2]=="Con");
-    REQUIRE(str[3]=="Cancer");
+    REQUIRE(str[2]=="Cancer");
+    REQUIRE(str[3]=="Con");
     REQUIRE(str[4]=="Petitbite");
     REQUIRE(str[5]=="");
-    //dictionnaire ordre:petitbite,cancer,con
+}
+TEST_CASE("One Controller","[controller][server]"){
+    vector<string> str;
+    vector<string>input;
+    input.push_back("MA v1.0");
+    input.push_back("CHECK DISEASES");
+    input.push_back("Cancer");
+    input.push_back("CCCC;TATA");
+    input.push_back("");
+    str=OneController().doOneEvaluation(input,"E:\\INSA\\3A\\GI UML\\tp_clion\\INSA-GL-Analyse-medicale\\res\\dictionnaire");
+    REQUIRE(str[0]=="MA v1.0");
+    REQUIRE(str[1]=="DISEASE Cancer");
+    //REQUIRE(str[2]=="");
+    REQUIRE(str[2]=="1");
+    REQUIRE(str[3]=="");
+}
+TEST_CASE("All Controller","[controller][server]"){
+    vector<string> str;
+    vector<string>input;
+    input.push_back("MA v1.0");
+    input.push_back("CHECK ALL");
+    input.push_back("CCCC;TATA;TTAA");
+    //input.push_back("TATA");
+    //input.push_back("");
+    str=AllController("E:\\INSA\\3A\\GI UML\\tp_clion\\INSA-GL-Analyse-medicale\\res\\dictionnaire").doCompleteEvaluation(input);
+    REQUIRE(str[0]=="MA v1.0");
+    REQUIRE(str[1]=="DISEASE Cancer");
+
+    REQUIRE(str[2]=="");
 }
