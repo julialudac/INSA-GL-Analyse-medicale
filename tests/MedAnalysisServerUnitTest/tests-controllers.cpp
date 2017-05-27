a96aaa585b39f6e241f15018c0e7325917086b00
Marc-Antoine Fernandes <embraser01@gmail.com>
2017-05-27 10:26:47 +0200

    Fix function name

--- tests/MedAnalysisServerUnitTest/tests-controllers.cpp
+++ tests/MedAnalysisServerUnitTest/tests-controllers.cpp
@@ -1,6 +1,5 @@
 #include <catch.hpp>
 #include <utils/utils.h>
-#define MEDANALYSIS_TEST
 #include <controllers/ListController.h>
 #include <controllers/OneController.h>
 #include <controllers/AllController.h>
@@ -9,50 +8,50 @@ using Catch::Matchers::Vector::ContainsElementMatcher;
 using std::string;
 using std::vector;
 
-TEST_CASE("List Controller", "[controller][server]"){
+TEST_CASE("List Controller", "[controller][server]") {
     vector<string> str;
-    vector<string>input;
+    vector<string> input;
     input.push_back("MA v1.0");
     input.push_back("GET DISEASES");
     input.push_back("");
 
     str = ListController("../../../res/dictionnaire").doGetList(input);
-    REQUIRE(str[0]=="MA v1.0");
-    REQUIRE(str[1]=="DISEASES");
+    REQUIRE(str[0] == "MA v1.0");
+    REQUIRE(str[1] == "DISEASES");
     //REQUIRE(str[2]=="");
-    REQUIRE(str[2]=="Cancer");
-    REQUIRE(str[3]=="Con");
-    REQUIRE(str[4]=="Petitbite");
-    REQUIRE(str[5]=="");
+    REQUIRE(str[2] == "Cancer");
+    REQUIRE(str[3] == "Con");
+    REQUIRE(str[4] == "Petitbite");
+    REQUIRE(str[5] == "");
 }
 
-TEST_CASE("One Controller","[controller][server]"){
+TEST_CASE("One Controller", "[controller][server]") {
     vector<string> str;
-    vector<string>input;
+    vector<string> input;
     input.push_back("MA v1.0");
     input.push_back("CHECK DISEASES");
     input.push_back("Cancer");
     input.push_back("CCCC;TATA");
     input.push_back("");
-    str=OneController().doOneEvaluation(input,"../../../res/dictionnaire");
-    REQUIRE(str[0]=="MA v1.0");
-    REQUIRE(str[1]=="DISEASE Cancer");
+    str = OneController().doOneEvaluation(input, "../../../res/dictionnaire");
+    REQUIRE(str[0] == "MA v1.0");
+    REQUIRE(str[1] == "DISEASE Cancer");
     //REQUIRE(str[2]=="");
-    REQUIRE(str[2]=="1");
-    REQUIRE(str[3]=="");
+    REQUIRE(str[2] == "1");
+    REQUIRE(str[3] == "");
 }
 
-TEST_CASE("All Controller","[controller][server]"){
+TEST_CASE("All Controller", "[controller][server]") {
     vector<string> str;
-    vector<string>input;
+    vector<string> input;
     input.push_back("MA v1.0");
     input.push_back("CHECK ALL");
     input.push_back("CCCC;TATA;TTAA");
     //input.push_back("TATA");
     //input.push_back("");
-    str=AllController("../../../res/dictionnaire").doCompleteEvaluation(input);
-    REQUIRE(str[0]=="MA v1.0");
-    REQUIRE(str[1]=="DISEASE Cancer");
+    str = AllController("../../../res/dictionnaire").doCompleteEvaluation(input);
+    REQUIRE(str[0] == "MA v1.0");
+    REQUIRE(str[1] == "DISEASE Cancer");
 
-    REQUIRE(str[2]=="");
+    REQUIRE(str[2] == "");
 }
--- tests/MedAnalysisServerUnitTest/tests-services.cpp
+++ tests/MedAnalysisServerUnitTest/tests-services.cpp
@@ -2,45 +2,41 @@
 #include <models/Genome.h>
 #include <services/AnalysisService.h>
 
-TEST_CASE( "Evaluating All Diseases", "[server][service]" )
-{
+TEST_CASE("Evaluating All Diseases", "[server][service]") {
     AnalysisService service = AnalysisService("../../../res/disease");
     vector<string> genes;
     genes.push_back("GTAC");
     genes.push_back("GGCC");
     Genome genome = Genome(genes);
     vector<Disease> diseases = service.evaluateAll(genome);
-    REQUIRE(diseases[0].name()=="Cancer");
+    REQUIRE(diseases[0].name() == "Cancer");
 }
 
-TEST_CASE( "Evaluating One Disease", "[server][service]" )
-{
+TEST_CASE("Evaluating One Disease", "[server][service]") {
     AnalysisService service = AnalysisService("../../../res/disease");
     vector<string> genes;
     genes.push_back("GTAC");
     genes.push_back("GGCC");
-    Disease disease = Disease("Cancer",genes);
+    Disease disease = Disease("Cancer", genes);
     Genome genome = Genome(genes);
-    REQUIRE(service.evaluateOne(genome,disease)==true);
+    REQUIRE(service.evaluateOne(genome, disease));
 }
 
-TEST_CASE( "Getting disease by name", "[server][service]" )
-{
+TEST_CASE("Getting disease by name", "[server][service]") {
     AnalysisService service = AnalysisService("../../../res/disease");
-    pair<   unordered_multimap<string, Disease>::iterator,
+    pair<unordered_multimap<string, Disease>::iterator,
             unordered_multimap<string, Disease>::iterator
     > disease = service.getDiseaseByName("Cancer");
-    REQUIRE(disease.first->first=="Cancer");
-    REQUIRE(disease.first->second.name()=="Cancer");
-    REQUIRE(disease.first->second.riskfulGenes()[0]=="GTAC");
-    REQUIRE(disease.first->second.riskfulGenes()[1]=="GGCC");
+    REQUIRE(disease.first->first == "Cancer");
+    REQUIRE(disease.first->second.name() == "Cancer");
+    REQUIRE(disease.first->second.riskfulGenes()[0] == "GTAC");
+    REQUIRE(disease.first->second.riskfulGenes()[1] == "GGCC");
 }
 
-TEST_CASE( "Getting diseases", "[server][service]" )
-{
+TEST_CASE("Getting diseases", "[server][service]") {
     AnalysisService service = AnalysisService("../../../res/disease");
-    auto diseasesMap = service.getDisease();
-    unordered_multimap<std::string,Disease>::iterator iter;
+    auto diseasesMap = service.getDiseases();
+    unordered_multimap<std::string, Disease>::iterator iter;
     vector<string> diseases;
     //diseases on the iterator qre on the opposite order
     diseases.push_back("YYDSB");
@@ -48,9 +44,8 @@ TEST_CASE( "Getting diseases", "[server][service]" )
     diseases.push_back("Cancer");
 
     int i = 0;
-    for(iter=diseasesMap.begin();iter!=diseasesMap.end();iter++)
-    {
-        REQUIRE(iter->first==diseases[i]);
+    for (iter = diseasesMap.begin(); iter != diseasesMap.end(); iter++) {
+        REQUIRE(iter->first == diseases[i]);
         i++;
     }
 }
